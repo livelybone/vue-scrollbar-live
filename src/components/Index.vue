e44f5878bcc0f2d80cade5becf72050461863871
livelybone <2631541504@qq.com>
2019-08-03 17:56:31 +0800

    feat(style): Update

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -191,11 +191,11 @@ export default {
         const style = document.createElement('style')
         style.id = id
         style.innerText =
-          '.scrollbar-content::-webkit-scrollbar{width:0;height:0;}' +
-          '.scrollbar-content{-ms-overflow-style:none;scrollbar-width:none;}' +
-          '.scrollbar{position:absolute;border-radius:.25em;background:#eee;box-shadow:0 0 2px rgba(0,0,0,0.1);opacity:0;pointer-events:none}' +
-          '.scrollbar-y{right:0.25em;width:.5em}' +
-          '.scrollbar-x{bottom:0.25em;height:.5em}' +
+          '.scrollbar-wrap .scrollbar-content::-webkit-scrollbar{width:0;height:0;}' +
+          '.scrollbar-wrap .scrollbar-content{-ms-overflow-style:none;scrollbar-width:none;}' +
+          '.scrollbar-wrap .scrollbar{position:absolute;border-radius:.25em;background:#eee;box-shadow:0 0 2px rgba(0,0,0,0.1);opacity:0;pointer-events:none}' +
+          '.scrollbar-wrap .scrollbar-y{right:0.25em;width:.5em}' +
+          '.scrollbar-wrap .scrollbar-x{bottom:0.25em;height:.5em}' +
           '.scrollbar-wrap:hover .scrollbar{opacity:1;pointer-events:initial}'
         document.head.appendChild(style)
       }
